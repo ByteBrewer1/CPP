ea7f01f156c5cedbefc45d98c95d36d6ed84ffe2
iusenotepadonly <22cs042@charusat.edu.in>
2023-04-04 09:21:42 +0530

    4April

--- Work@Tech/Two Pointers/UniqueElementsinSortedArray.cpp
+++ Work@Tech/Two Pointers/UniqueElementsinSortedArray.cpp
@@ -8,33 +8,20 @@ public:
 
 int Solution::removeDuplicates(vector<int> &A)
 {
-    int i = 0; // Erasing approach
-    int len = A.size();
-    while (i < A.size())
+    int n = A.size();
+    if (n <= 1)
+        return n;
+
+    int j = 0;
+    for (int i = 1; i < n; i++)
     {
-        int j = i + 1;
-        if (A[i] == A[j])
+        if (A[i] != A[j])
         {
-            ++j;
-            while (A[i] == A[j] && j < len)
-                ++j;
-            --j;
-            len -= (j - 1 - i);
-            A.erase(A.begin() + i + 1, A.begin() + j);
+            j++;
+            A[j] = A[i];
         }
-        ++i;
     }
-    return len;
-    int count = 0; // Just moving duplicates at back of vector approach, faster;
-    int size = A.size();
-    for (auto i = 0; i < size; ++i)
-    {
-        if (i < size - 2 && A[i] == A[i + 1] && A[i] == A[i + 2])
-            continue;
-        else
-            A[count++] = A[i];
-    }
-    return count;
+    return j + 1;
 }
 
 int main()
